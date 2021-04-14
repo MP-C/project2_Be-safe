f96487d83e5fc5922ab171d28e40b95155f81085
xdewez <77564747+xdewez@users.noreply.github.com>
2021-04-14 13:26:27 +0200

    création branche Header

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="banner-container">
+            <h1>Statistiques et Mesures Covid</h1>
+        </div>
+    );
+}
+
+export default Header;
