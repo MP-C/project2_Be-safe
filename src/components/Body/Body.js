d859df204c8615a82e1c913e2b74cc1bac296f23
Pauline De Ro <pauline.dero@gmail.com>
2021-04-14 16:01:12 +0200

    modified body.js

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,9 +3,22 @@ import './Body.css';
 
 export default function Body (){
     return (
-        <div>
-            
+        <div id="body">
+            <h1> Voici mon composant body</h1>
+            <img id="logo-BeSafe"/>
+            <div id="choose-geoloc-filter">
+                <button id="geolocalisation">Se géolocaliser</button>
+                <button id="choose-city">Sélectionner une commune</button>
+                <div id="input-filter">
+                    <input name="city" />
+                    <input name="date" />
+                </div>
+            </div>
+            <div id="map">
+                <img id="map-result"/>
+            </div>
+            <div id="search-result">
+            </div>
         </div>
     )
-    
 }
