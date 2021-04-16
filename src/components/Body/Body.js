18062de9e926b21a4c3ca01b82525ab538bc93c2
Pauline De Ro <pauline.dero@gmail.com>
2021-04-16 13:51:59 +0200

    continue development body.js

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import './Body.css';
 import logo from './Be-Safe-logo.png';
+import carte from './carte.png';
 
 export default function Body() {
     return (
         <div id="body">
-            <h1> Voici mon composant body</h1>
-            <div id="column-left">
-                <img id="logo-BeSafe" src={logo} width="150px" class="logo" />
+            <div id="column-left" className="column">
+                <img
+                    id="logo-BeSafe"
+                    src={logo}
+                    width="150px"
+                    className="logo vertical-margin"
+                />
                 <div id="choose-geoloc-filter">
-                    <button id="geolocalisation">Se géolocaliser</button>
-                    <div id="input-filter">
-                        <input name="city" />
-                        <input name="date" />
-                        <button id="choose-city">Sélectionner une commune</button>
+                    <button id="geolocalisation" class="input bottom-margin">
+                        Se géolocaliser
+                    </button>
+                    <div id="input-filter" class="filter">
+                        <input
+                            name="city"
+                            class="input top-margin"
+                            placeholder="Commune"
+                            type="text"
+                        />
+                        <input
+                            name="date"
+                            class="input top-margin"
+                            placeholder="Date"
+                            type="date"
+                        />
+                        <button id="choose-city" class="input top-margin">
+                            Sélectionner une commune
+                        </button>
                     </div>
                 </div>
             </div>
-            <div id="column-right">
-                <img id="map-result" />
+            <div id="column-right" class="map">
+                <img id="map-result" src={carte} />
             </div>
             <div id="search-result">
+                <p>Cas détectés</p>
+                <p>Hospitalisations</p>
+                <p>Décès</p>
             </div>
         </div>
-    )
+    );
 }
