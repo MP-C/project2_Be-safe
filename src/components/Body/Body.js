e84b6fc5a5271bfd8a9dd8826026d6b0b6c062c3
Pauline De Ro <pauline.dero@gmail.com>
2021-04-16 10:49:18 +0200

    update body.js body.css

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './Body.css';
+import logo from './Be-Safe-logo.png';
 
-export default function Body (){
+export default function Body() {
     return (
         <div id="body">
             <h1> Voici mon composant body</h1>
-            <img id="logo-BeSafe"/>
-            <div id="choose-geoloc-filter">
-                <button id="geolocalisation">Se géolocaliser</button>
-                <button id="choose-city">Sélectionner une commune</button>
-                <div id="input-filter">
-                    <input name="city" />
-                    <input name="date" />
+            <div id="column-left">
+                <img id="logo-BeSafe" src={logo} width="150px" class="logo" />
+                <div id="choose-geoloc-filter">
+                    <button id="geolocalisation">Se géolocaliser</button>
+                    <div id="input-filter">
+                        <input name="city" />
+                        <input name="date" />
+                        <button id="choose-city">Sélectionner une commune</button>
+                    </div>
                 </div>
             </div>
-            <div id="map">
-                <img id="map-result"/>
+            <div id="column-right">
+                <img id="map-result" />
             </div>
             <div id="search-result">
             </div>
