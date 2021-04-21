2ac8dd3bf955df202eb975042dee2a97615056d3
xdewez <77564747+xdewez@users.noreply.github.com>
2021-04-21 08:50:49 +0200

    Creation du bouton Géolocalisation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
+import Geoloc from "./Components/Geoloc";
 import "./App.css";
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Geoloc />
     </div>
   );
 }
---
+++ src/Components/Geoloc.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './Geoloc.css';
+
+  const Geoloc = () => {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus('Geolocation is not supported by your browser');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        setStatus('Unable to retrieve your location');
+      });
+    }
+  }
+
+  return (
+    <div className="Geoloc">
+      <button className='button-geo' onClick={getLocation}><h3>Get Location</h3></button>
+      <h1>Coordonées</h1>
+      <p>{status}</p>
+      {lat && <p>Latitude: {lat}</p>}
+      {lng && <p>Longitude: {lng}</p>}
+    </div>
+  );
+}
+
+export default Geoloc;
