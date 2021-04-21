268a0d5e0f8ef6d438315e3f6aa55f1ae99a3480
MP-C <mp.crcarvalho@gmail.com>
2021-04-21 16:03:54 +0200

    footer presque Css sans logo

---
+++ src/components/HeaderBodyExemple.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./HeaderBodyExemple.css";
+
+export default function HeaderBodyExemple() {
+
+    return <div className="HeaderBodyExemple"> It's the body </div>
+};
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,36 +1,45 @@
 import React from "react";
-import "./footer.css";
-import logo_be from "./logo_footer/logo_be.jpg";
-import logo_brussels_covid from "./logo_footer/logo_brussels_covid.jpeg";
-import logo_sciensano from "./logo_footer/logo_sciensano.png";
+import "./Footer.css";
+import LogoBe from "./LogoFooter/LogoBe.jpg";
+import LogoBrusselsCovid from "./LogoFooter/LogoBrusselsCovid.jpeg";
+import LogoSciensano from "./LogoFooter/LogoSciensano.png";
 
+export default function Footer() {
 
-export default function footer() {
-
-
-
-    return {
-        <form>
-        <div id=footer>
-        <div className="footer">
-            <div className="footer_generalInformations " id="footer_generalInformations">
-                <h3 className="footer_title" id> Importantes Constacts</h3>
-                <div>
-                    <h3 className=""> Importantes Constacts</h3>
-
-
-                    <div className="footer_importantsContacts">
-
-
-                    </div>
-                    <div>
-                        <img className="logo_contact_be" src={logo_be} alt="" />
-                        <img className="logo_contact_be" src={logo_brussels_covid} alt="" />
-                        <img className="logo_contact_be" src={logo_sciensano} />
-                    </div>
-                </div>
+  return (
+    <div className="footer">
+      <div className="footer_generalInformations">
+        <h3 className="footer_title"> General Informations</h3>
+        <div className="footer_contactsAndSites">
+          <div className="footer_importantsContacts" >
+            <h5 className="footer_ImportantesConstactsTitle"> Importantes Contacts</h5>
+            <h6 className="contactList">Call center Belgium: 0800/14.689 </h6>
+            <h5 className="contactList">Call center Bruxelles : 0800/35.243 </h5>
+          </div>
+          <div calssName="footer_SitesInformations">
+            <h3>National Informations Sites</h3>
+            <div className="network_logos">
+              <ul>
+                <a href="www.info-coronavirus.be">
+                  <div>
+                  <p>Belgium <img className="logo_contact_be_belgium" src={LogoBe} alt="" /></p>
+                  </div>
+                </a>
+                <a href="www.coronavirus.brussels">
+                  <div>
+                    <p>Brussels <img className="logo_contact_be_brussels" src={LogoBrusselsCovid} alt="" /></p>
+                  </div>
+                </a>
+                <a href="www.datastudio.google.com/u/0/reporting/c14a5cfc-cab7-4812-848c-0369173148ab/page/hOMwB">
+                  <div>
+                  <p>Sciensano <img className="logo_contact_be_Sciensano" src={LogoSciensano} alt="" /></p>
+                  </div>
+                </a>
+              </u>
             </div>
+          </div>
         </div>
-        </form>`
-    }
+      </div>
+    </div>
+  )
 }
