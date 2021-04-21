520ead8e414f0883c0b1fcb3c0f07becbffc9f73
xdewez <77564747+xdewez@users.noreply.github.com>
2021-04-21 15:59:21 +0200

    suppression composant Geoloc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Components/Header";
-import Geoloc from "./Components/Geoloc";
 import Map from "./Components/Map";
 
 import "./App.css";
@@ -10,7 +9,6 @@ function App() {
   return (
     <div>
       <Header />
-      <Geoloc />
       <Map />
     </div>
   );
