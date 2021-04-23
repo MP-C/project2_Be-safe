845d28af436e599df369a496b5862528bc4d8c62
MP-C <mp.crcarvalho@gmail.com>
2021-04-23 09:45:48 +0200

    new files name js and css Footer post error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Body from './Components/Body/Body';
 import Header from './Components/Header';
 import Map from './Components/Map';
-import Footer from './Components/Footer';
+import Footer from './Components/Footer1';
 
   export default function App() {
 
