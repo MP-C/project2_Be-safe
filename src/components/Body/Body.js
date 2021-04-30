a3995cf4ded135fa1d7f807ffdcdeb43ef3b32f5
MP-C <mp.crcarvalho@gmail.com>
2021-04-30 12:24:45 +0200

    updates api mario correction

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
-//  import axios from 'axios';
-
+// import axios from 'axios';
 import Map from '../Map/Map';
-
 import './Body.css';
-
 import logo from '../assets/Be-Safe-Logo.png';
 
 export default function Body() {
     const [city, setCity] = useState('');
-
     //  const getCovidDataPerCity = () => {
     //  axios
     //  .get('')
@@ -20,8 +16,35 @@ export default function Body() {
     //  };
     useEffect(() => { }, [city]);
 
-    const [date, setDate] = useState('');
+    const [cases, setCases] = useState('');
+    
+    const getCovidDataPerCity = () => {
+        fetch('https://data.opendatasoft.com/api/records/1.0/search/?dataset=covid-19-pandemic-belgium-cases-municipality%40public&q=&sort=date&facet=date&facet=nis5&facet=tx_descr_nl&facet=tx_descr_fr&facet=tx_adm_dstr_descr_nl&facet=tx_adm_dstr_descr_fr')
+            .then((response) => response.data)
+            .then((data) => {
+                
+                setCases(data.records[0].fields.geo_point_2d);
+            });
+    };
+    console.log(getCovidDataPerCity);
 
+    useEffect(() => { }, [cases]);
+
+    /*
+    const [hospitalisation, setHospitalisation]= useState (''); //use records from hospistalisation
+    const regionDeath=[];//to stock info (Where Regions in both API's are equals) + Hospitalisations 
+    const getHospitalisationsDataPerRegion = () => { //get hospitalisations IF city in 1API == 2API
+        axios
+        .get('https://data.opendatasoft.com/api/records/1.0/search/?dataset=covid-19-pandemic-belgium-hosp-province%40public&q=&sort=date&facet=date&facet=province&facet=region')
+        .then ((response) => response.data)
+        .then ((data) => {
+        setHospitalisation();
+        console.log 
+        });
+        };
+    */
+
+    const [date, setDate] = useState('');
     return (
         <div id="body">
             <div id="column-left" className="column">
@@ -38,7 +61,7 @@ export default function Body() {
                     >
                         Se géolocaliser
                     </button>
-                    <div id="input-filter" class="filter">
+                    <div id="input-filter" className="filter">
                         <input
                             name="city"
                             className="input top-margin"
@@ -58,6 +81,7 @@ export default function Body() {
                         <button
                             id="choose-city"
                             className="top-margin button bottom-margin"
+                            onClick={getCovidDataPerCity}
                         >
                             Sélectionner une commune
                         </button>
@@ -65,17 +89,19 @@ export default function Body() {
                             <h4>Votre rercherche :</h4>
                             <p> Ville : {city} </p>
                             <p> Date : {date} </p>
+                            <p>{cases}</p>
+                            
+                        </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="column-right" className="map">
-                <div id="search-result">
-                    <Map />
+                <div id="column-right" className="map">
+                    <div id="search-result">
+                        <Map />
+                    </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
     );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,41 +7,41 @@ import LogoBrusselsCovid from '../assets/LogoBrusselsCovid.png';
 import LogoSciensano from '../assets/LogoSciensano.png';
 
 export default function Footer() {
- 
-    return (
-      <div className="footer">
-        <div className="footer_generalInformations">
-          <h2 className="footer_title"> General Informations</h2>
-          <div className="footer_contactsAndSites">
-            <div className="footer_importantsContacts" >
-              <h4 className="footer_titleImportantesConstacts"> Importantes Contacts</h4>
-              <div className="titleContacts">
-                <h5 className="titleContactList">Call center Belgium: 0800/14.689</h5>
-                <h5 className="titleContactList">Call center Bruxelles : 0800/35.243</h5>
-              </div>
+
+  return (
+    <div className="footer">
+      <div className="footer_generalInformations">
+        <h2 className="footer_title"> General Informations</h2>
+        <div className="footer_contactsAndSites">
+          <div className="footer_importantsContacts" >
+            <h4 className="footer_titleImportantesConstacts"> Importantes Contacts</h4>
+            <div className="titleContacts">
+              <h5 className="titleContactList">Call center Belgium: 0800/14.689</h5>
+              <h5 className="titleContactList">Call center Bruxelles : 0800/35.243</h5>
             </div>
-            <div calssName="footer_SitesInformations">
-              <h4 className="titleSitesInformations"> National Informations Sites</h4>
-              <div className="network_logos">
-                  <div>
-                    <a href="https://www.info-coronavirus.be">
-                    <img className="logo_contact_be_belgium" src={LogoBe} alt="" />
-                    </a>
-                  </div>
-                  <div>
-                    <a href="https://www.coronavirus.brussels">
-                      <img className="logo_contact_be_brussels" src={LogoBrusselsCovid} alt="" />
-                    </a>
-                  </div>
-                  <div>
-                    <a href="https://epistat.wiv-isp.be/covid/covid-19.html">
-                      <img className="logo_contact_be_Sciensano" src={LogoSciensano} alt="" />
-                    </a>
-                  </div>
+          </div>
+          <div className="footer_SitesInformations">
+            <h4 className="titleSitesInformations"> National Informations Sites</h4>
+            <div className="network_logos">
+              <div>
+                <a href="https://www.info-coronavirus.be">
+                  <img className="logo_contact_be_belgium" src={LogoBe} alt="" />
+                </a>
+              </div>
+              <div>
+                <a href="https://www.coronavirus.brussels">
+                  <img className="logo_contact_be_brussels" src={LogoBrusselsCovid} alt="" />
+                </a>
+              </div>
+              <div>
+                <a href="https://epistat.wiv-isp.be/covid/covid-19.html">
+                  <img className="logo_contact_be_Sciensano" src={LogoSciensano} alt="" />
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
