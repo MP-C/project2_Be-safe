fbd6af374353ac0f4d4c357934af35a370fd8d5d
Pauline De Ro <pauline.dero@gmail.com>
2021-05-06 16:10:06 +0200

    try NOK pull request

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Map from '../Map/Map';
 import './Body.css';
 
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
-=======
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
->>>>>>> e8aaa1931694f07aabfa6823d6264fa4c9c34972
 import './Map.css';
 
 export default function Map({ lat, lng }) {
