ac36bb1d87e7b2df83748585d098603f14c01f1c
Pauline De Ro <pauline.dero@gmail.com>
2021-05-06 17:08:18 +0200

    Add info button and linked CSS

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import './Header.css';
 import logo from '../assets/Be-Safe-Logo.png';
 
 export default function Header() {
     return (
         <div className="header_intro">
-            <div className="banner-container">
-                <a className="logo" href="index.js">
-                    <img className="logo-Be-Safe" src={logo} />
-                </a>
-                <div className="headear_text">
-                    <p className="headear_text_presentation">
+            <div className="header_text">
+                <Popup
+                    trigger={<button className="info-button"> ? </button>}
+                    position="left top"
+                >
+                    <div>
                         This site alows you to consult any information about
                         COVID-19 statistics developpement around the world. It
                         uses outsourcing free API, to identify the lastest news
                         (total deaths, total recovered and total contaminations
                         confirmed).
-                    </p>
-                </div>
+                    </div>
+                </Popup>
+            </div>
+            <div className="banner-container">
+                <a className="logo" href="index.js">
+                    <img className="logo-Be-Safe" src={logo} />
+                </a>
             </div>
         </div>
     );
